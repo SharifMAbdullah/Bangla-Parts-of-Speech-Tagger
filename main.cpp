431f0b8c02abf11bf7e20ace76eac00ef57300fa
SharifMAbdullah <bsse1211@iit.du.ac.bd>
2022-02-17 19:59:11 +0600

    impleneted tokeniser

--- main.cpp
+++ main.cpp
@@ -1,24 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
+
+bool isPunctuation(wchar_t c)
+{
+    if(c == L' ')
+        return 1; 
+    else if (c ==  L'।')
+        return 1; 
+    else if (c == L'।')
+        return 1; 
+    else if (c == L',')
+        return 1;
+    else if (c == L'?')
+        return 1;
+return 0;
+}
+
+void tokeniser(vector<wstring> s)
 {
     wstring str,temp=L"";
-    std::vector<wstring> store;
     wfstream f1,f2;
     //FILE *ptr = fopen("read.txt","r");
-    if(!f1 && !f2)
+    f1.open("read.txt");
+    f2.open("write.txt");
+
+    if(!f1 || !f2)
         cout << "File not found!" << endl;
     else
     {
-    f1.open("read.txt");
-    f2.open("write.txt");
-    getline(f1,str);
-    int j=0;
-    for(int i=0;i<str.size();i++)
-    {
-        if(str[i] == ' ' || str[i] ==  L'।' || str[i] == L'।' || str[i] == ',')
+        getline(f1,str);
+        int j=0;
+        for(int i=0;i<str.size();i++)
+        {
+            if(isPunctuation(str[i]))
             {
-                store.push_back(temp);
+                s.push_back(temp);
                 temp = L"";
                
             }
@@ -28,8 +44,14 @@ int main()
                 //cout << "Found delimeter\n";
             }
     }
-    store.push_back(temp);
-    for(int i=0;i<str.size();i++)
-    f2 << store[i] << endl;
+    s.push_back(temp);
+    for(int i=0;i<s.size();i++)
+    f2 << s[i] << endl;
     }
+}
+
+int main()
+{
+    std::vector<wstring> store;
+    tokeniser(store);
 }
