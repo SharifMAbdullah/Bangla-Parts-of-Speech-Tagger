64c02a091f172c8e983540609bbcfb486f20d85c
SharifMAbdullah <bsse1211@iit.du.ac.bd>
2022-02-20 14:50:57 +0600

    issues solved

--- main.cpp
+++ main.cpp
@@ -2,32 +2,53 @@
 #include <codecvt>
 #include <locale>
 #include <fcntl.h>
+#include<uchar.h>
 using namespace std;
 
-bool isPunctuation(char16_t c)
+bool isDari(wchar_t c)
 {
-    if(c == u' ')
-        return 1; 
-    else if (c ==  u'।')
-        return 1; 
-    else if (c == u'।')
-        return 1; 
-    else if (c ==  u'\r')
+    //std::locale::global(std::locale(""));
+    wchar_t ch = L'\u0964';
+    if (c == ch)
+    return 1;
+return 0;
+}
+
+bool isPunctuation(wchar_t c)
+{
+    //std::locale::global(std::locale(""));
+    wchar_t ch = L'\u0964';
+    if (c == ch)
         return 1;
-    else if (c ==  u'\n')
+    //if(iswspace(c))
+        //return 1;    
+    //if(iswblank(c))
+        //return 1;
+    //if(iswpunct(c))
+        //return 1;
+    else if(c == L' ')
+        return 1; 
+    //else if (u_isalpha(।))
+        //return 1; 
+    //else if (c == L'।')
+        //return 1; 
+    else if (c ==  L'\r')
         return 1;
-    else if (c == u',')
+    else if (c ==  L'\n')
         return 1;
-    else if (c == u'?')
+    else if (c == L',')
+        return 1;
+    else if (c == L'?')
         return 1;    
 return 0;
 }
 
-void tokeniser(vector<string> s)
+void tokeniser(vector<wstring> s)
 {
- 
-    string str,temp="";
-    fstream f1,f2;
+
+    //std::locale::global(std::locale(""));
+    wstring str,temp=L"";
+    wfstream f1,f2;
     /*f1.imbue(utf16_locale);
     f2.imbue(utf16_locale);
     _setmode(_fileno(stdout), _O_U16TEXT);*/
@@ -48,8 +69,7 @@ void tokeniser(vector<string> s)
                     if(isPunctuation(str[i]))
                     {
                     s.push_back(temp);
-                    temp = "";
-               
+                    temp = L"";
                     }
                     else
                     {
@@ -67,6 +87,7 @@ void tokeniser(vector<string> s)
 
 int main()
 {
-    std::vector<string> store;
+    //setlocale(LC_ALL, "en_US.utf8");
+    std::vector<wstring> store;
     tokeniser(store);
 }
