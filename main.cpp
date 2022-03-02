319f9011dd5df3e303a6eb676ff3625ec3384bb9
SharifMAbdullah <bsse1211@iit.du.ac.bd>
2022-03-02 16:41:55 +0600

    still problem

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include<vector>
-#include <cstring> 
+#include <cstring>
 #define max 30
 
 using namespace std;
@@ -11,51 +11,52 @@ string strings[max];
 bool isPunctuation(char c)
 {
      if(c == ' ')
-         return 1; 
+         return 1;
      else if (c ==  '?')
-         return 1; 
+         return 1;
      else if (c == '#')
-         return 1; 
+         return 1;
      else if (c == ',')
          return 1;
      else if (c == '-')
-         return 1;        
-
+         return 1;
+    else if (c == '\n' || c == '\r')
+         return 1;
 return 0;
 }
 
-int len(string str)  
-{  
-    int length = 0;  
-    for (int i = 0; str[i] != '\0'; i++)  
-    {  
-        length++;  
+int len(string str)
+{
+    int length = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        length++;
         // cout<<str[i]<<endl;
-        // cout<<(int)str[i]<<endl;  
-    }  
-    return length;     
+        // cout<<(int)str[i]<<endl;
+    }
+    return length;
 }
 
-int split (string str, char seperator)  
-{  
-    int  currentIndex=0, i = 0;  
-    int startIndex = 0, endIndex = 0;  
-    while (i <= len(str))  
-    {  
-        if (str[i] == seperator || i == len(str))  
-        {  
-            endIndex = i;  
-            string subStr = "";  
-            subStr.append(str, startIndex, endIndex - startIndex);  
-            strings[currentIndex] = subStr;  
-            currentIndex += 1;  
-            startIndex = endIndex + 1;  
-        }  
-        i++;  
-        
+int split (string str, char seperator)
+{
+    int  currentIndex=0, i = 0;
+    int startIndex = 0, endIndex = 0;
+    while (i <= len(str))
+    {
+        if (str[i] == seperator || i == len(str))
+        {
+            endIndex = i;
+            string subStr = "";
+            subStr.append(str, startIndex, endIndex - startIndex);
+            strings[currentIndex] = subStr;
+            currentIndex += 1;
+            startIndex = endIndex + 1;
         }
-    return currentIndex;     
-} 
+        i++;
+
+    }
+    return currentIndex;
+}
 
 
 void tokeniser() //vector<string> s
@@ -72,12 +73,15 @@ void tokeniser() //vector<string> s
     {
         char ch;
         int i = 0;
-		
+
         string tp;
         while(getline(f1, tp))
-        { 
+        {
             //read data from file object and put it into string.
             //int currentIndex = 0;
+            string rmv = "\n";
+            //if (rmv.compare(0, rmv.size(), tp, 0, tp.size()) == 0)
+                //tp="ok";
 
             string str1 = "৷";
             int currentIndex = split(tp, ' ');
@@ -88,7 +92,7 @@ void tokeniser() //vector<string> s
                 if (found != string::npos)
                 {
                     cout << "First occurrence is " << found << endl;
-        
+
                     strings[i].replace(found,3,"###");
                     cout<<strings[i]<<endl;
                 }
@@ -108,21 +112,21 @@ void tokeniser() //vector<string> s
                     }
                 }
 
-            strings[i].clear();
+            //strings[i].clear();
             f2 << endl;
-                
+
             }
-  
+
         }
 
       f1.close();
       f2.close();
     }
-    
+
 }
 
 int main() {
     //vector<string> s;
     tokeniser(); //s
-	
+
 }
